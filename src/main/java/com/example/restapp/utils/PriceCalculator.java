1c41968312b5bcf6cb4832fba29ee576482dab23
harsha <harshasandamal100@gmail.com>
2019-12-20 01:56:35 +0530

    Change code for accept environment variables

--- src/main/java/com/example/restapp/utils/PriceCalculator.java
+++ src/main/java/com/example/restapp/utils/PriceCalculator.java
@@ -1,5 +1,6 @@
 package com.example.restapp.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.text.DecimalFormat;
@@ -7,11 +8,16 @@ import java.text.DecimalFormat;
 @Component
 public class PriceCalculator {
 
-    private double PER_SMS_CHARGE = 0.12921 ;
-    private double MARKUP_PERCENTAGE = 50 ;
-    private double FIXED_PAYMENT_CHARGE = 0.3 ;
-    private double PAYMENT_PERCENTAGE = 0.029;
-    private double USD_TO_GBP_RATE = 0.76;
+    @Value("{DOCKER_PER_SMS_CHARGE: 0.12921}")
+    private String PER_SMS_CHARGE = "" ;
+    @Value(("{DOCKER_MARKUP_PERCENTAGE : 50 }"))
+    private String MARKUP_PERCENTAGE = "" ;
+    @Value(("{DOCKER_FIXED_PAYMENT_CHARGE : 0.3 }"))
+    private String FIXED_PAYMENT_CHARGE = "" ;
+    @Value(("{DOCKER_PAYMENT_PERCENTAGE : 0.029 }"))
+    private String PAYMENT_PERCENTAGE = "";
+    @Value(("{DOCKER_USD_TO_GBP_RATE : 0.76 }"))
+    private String USD_TO_GBP_RATE = "";
 
     /*
     gets initial AWS SMS costs based on the sms count and type
@@ -23,24 +29,24 @@ public class PriceCalculator {
             case 1: break;
             default:
                 // all days
-                return PER_SMS_CHARGE * 31 ;
+                return getPER_SMS_CHARGE() * 31 ;
         }
         return -1;
     }
 
     private double markupedPrice( double initial)
     {
-        return initial + (initial * MARKUP_PERCENTAGE)/100 ;
+        return initial + (initial * getMARKUP_PERCENTAGE())/100 ;
     }
 
     private double priceWithPaymentCharges( double markupvalue)
     {
-        return (markupvalue + FIXED_PAYMENT_CHARGE) * (1+ PAYMENT_PERCENTAGE);
+        return (markupvalue + getFIXED_PAYMENT_CHARGE()) * (1+ getPAYMENT_PERCENTAGE());
     }
 
     private double USDtoGBP(double usd) {
         DecimalFormat df = new DecimalFormat("#.##");
-        return Double.parseDouble(df.format(usd * USD_TO_GBP_RATE));
+        return Double.parseDouble(df.format(usd * getUSD_TO_GBP_RATE()));
     }
 
     public double getFinalPrice(int type)
@@ -54,5 +60,23 @@ public class PriceCalculator {
         return -1;
     }
 
+    public double getPER_SMS_CHARGE() {
+        return Double.parseDouble(PER_SMS_CHARGE);
+    }
 
+    public double getMARKUP_PERCENTAGE() {
+        return Double.parseDouble(MARKUP_PERCENTAGE);
+    }
+
+    public double getFIXED_PAYMENT_CHARGE() {
+        return Double.parseDouble(FIXED_PAYMENT_CHARGE);
+    }
+
+    public double getPAYMENT_PERCENTAGE() {
+        return Double.parseDouble(PAYMENT_PERCENTAGE);
+    }
+
+    public double getUSD_TO_GBP_RATE() {
+        return Double.parseDouble(USD_TO_GBP_RATE);
+    }
 }
